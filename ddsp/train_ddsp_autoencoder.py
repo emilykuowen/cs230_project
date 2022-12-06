72665bd6d6224e6d17a832c1e69791484c9377e7
Ubuntu <ubuntu@ip-172-31-8-221.us-west-2.compute.internal>
2022-12-06 04:37:59 +0000

    added ddsp model outputs

--- ddsp/train_ddsp_autoencoder.py
+++ ddsp/train_ddsp_autoencoder.py
@@ -64,7 +64,7 @@ ddsp_run_command = "ddsp_run \
   --gin_file=models/solo_instrument.gin \
   --gin_file=datasets/tfrecord.gin \
   --gin_param=\"TFRecordProvider.file_pattern='data/train.tfrecord*'\" \
-  --gin_param=\"batch_size=16\" \
+  --gin_param=\"batch_size=1\" \
   --gin_param=\"train_util.train.num_steps=30000\" \
   --gin_param=\"train_util.train.steps_per_save=300\" \
   --gin_param=\"trainers.Trainer.checkpoints_to_keep=10\""
