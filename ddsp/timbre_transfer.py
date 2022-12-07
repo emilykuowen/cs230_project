be5ba589efbf6c4254abdbe0597d9228b7d12f95
emilykuowen <emily.kuowen@gmail.com>
2022-12-06 16:34:12 -0800

    added ddsp model running code

--- ddsp/timbre_transfer.py
+++ ddsp/timbre_transfer.py
@@ -64,3 +64,67 @@ ax[2].plot(audio_features['f0_confidence'][:TRIM])
 ax[2].set_ylabel('f0 confidence')
 _ = ax[2].set_xlabel('Time step [frame]')
 plt.savefig('piano_audio_features.png')
+
+model_dir = "bass_model/"
+gin_file = os.path.join(model_dir, 'operative_config-0.gin')
+
+# Load the dataset statistics.
+DATASET_STATS = None
+dataset_stats_file = os.path.join(model_dir, 'dataset_statistics.pkl')
+print(f'Loading dataset statistics from {dataset_stats_file}')
+try:
+  if tf.io.gfile.exists(dataset_stats_file):
+    with tf.io.gfile.GFile(dataset_stats_file, 'rb') as f:
+      DATASET_STATS = pickle.load(f)
+except Exception as err:
+  print('Loading dataset statistics from pickle failed: {}.'.format(err))
+
+# Parse gin config,
+with gin.unlock_config():
+  gin.parse_config_file(gin_file, skip_unknown=True)
+
+# Assumes only one checkpoint in the folder, 'ckpt-[iter]`.
+ckpt_files = [f for f in tf.io.gfile.listdir(model_dir) if 'ckpt' in f]
+ckpt_name = ckpt_files[0].split('.')[0]
+ckpt = os.path.join(model_dir, ckpt_name)
+
+# Ensure dimensions and sampling rates are equal
+time_steps_train = gin.query_parameter('F0LoudnessPreprocessor.time_steps')
+n_samples_train = gin.query_parameter('Harmonic.n_samples')
+hop_size = int(n_samples_train / time_steps_train)
+
+time_steps = int(audio.shape[1] / hop_size)
+n_samples = time_steps * hop_size
+
+print("===Trained model===")
+print("Time Steps", time_steps_train)
+print("Samples", n_samples_train)
+print("Hop Size", hop_size)
+print("\n===Resynthesis===")
+print("Time Steps", time_steps)
+print("Samples", n_samples)
+print('')
+
+gin_params = [
+    'Harmonic.n_samples = {}'.format(n_samples),
+    'FilteredNoise.n_samples = {}'.format(n_samples),
+    'F0LoudnessPreprocessor.time_steps = {}'.format(time_steps),
+    'oscillator_bank.use_angular_cumsum = True',  # Avoids cumsum accumulation errors.
+]
+
+with gin.unlock_config():
+  gin.parse_config(gin_params)
+
+# Trim all input vectors to correct lengths 
+for key in ['f0_hz', 'f0_confidence', 'loudness_db']:
+  audio_features[key] = audio_features[key][:time_steps]
+audio_features['audio'] = audio_features['audio'][:, :n_samples]
+
+# Set up the model just to predict audio given new conditioning
+model = ddsp.training.models.Autoencoder()
+model.restore(ckpt)
+
+# Build model by running a batch through it.
+start_time = time.time()
+_ = model(audio_features, training=False)
+print('Restoring model took %.1f seconds' % (time.time() - start_time))
