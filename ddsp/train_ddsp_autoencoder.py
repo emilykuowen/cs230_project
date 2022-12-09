4b573c7028a14fb263d4d9bacd7e16f6abb8a83c
emilykuowen <emily.kuowen@gmail.com>
2022-12-08 21:52:52 -0800

    added changes to ddsp_autoencoder

--- ddsp/train_ddsp_autoencoder.py
+++ ddsp/train_ddsp_autoencoder.py
@@ -5,13 +5,13 @@ import tensorboard as tb
 
 # os.system("pip install -qU ddsp[data_preparation]==1.6.3")
 
-instrument = 'bass'
+instrument = 'piano'
 # Make directories to save model and data
-DRIVE_DIR = '../data/musdb18/train/' + instrument
-AUDIO_DIR = '../data/musdb18/tf_audio/' + instrument
+DRIVE_DIR = '../../data/medleydb_' + instrument + '/' + instrument
+AUDIO_DIR = '../../data/medleydb_' + instrument + '/tfaudio/'
 AUDIO_FILEPATTERN = AUDIO_DIR + '/*'
 os.system("mkdir -p " + AUDIO_DIR)
-SAVE_DIR = os.path.join(DRIVE_DIR, 'ddsp-solo-instrument')
+SAVE_DIR = os.path.join(DRIVE_DIR, 'ddsp')
 os.system("mkdir -p " + SAVE_DIR)
 
 mp3_files = glob.glob(os.path.join(DRIVE_DIR, '*.mp3'))
@@ -25,31 +25,33 @@ for fname in audio_files:
   os.system(cp_command)
 
 # Preprocess raw audio into TFRecord dataset
-TRAIN_TFRECORD = 'data/' + instrument + '/train.tfrecord'
+TRAIN_TFRECORD = '../../data/' + 'medleydb_' + instrument + 'tfrecord/train.tfrecord'
 TRAIN_TFRECORD_FILEPATTERN = TRAIN_TFRECORD + '*'
 
-# Copy dataset from drive if dataset has already been created.
-drive_data_dir = os.path.join(DRIVE_DIR, 'data')
-drive_dataset_files = glob.glob(drive_data_dir + '/*')
+# # Copy dataset from drive if dataset has already been created.
+# drive_data_dir = os.path.join(DRIVE_DIR, 'data')
+# drive_dataset_files = glob.glob(drive_data_dir + '/*')
 
-if DRIVE_DIR and len(drive_dataset_files) > 0:
-    os.system("cp " + drive_data_dir + "/* data/" + instrument)
-else:
-    # Make a new dataset.
-    if not glob.glob(AUDIO_FILEPATTERN):
-        raise ValueError('No audio files found. Please use the previous cell to upload.')
+# if DRIVE_DIR and len(drive_dataset_files) > 0:
+#     os.system("cp " + drive_data_dir + "/* data/" + instrument)
+# else:
 
-    ddsp_prepare_tfrecord_command = "ddsp_prepare_tfrecord \
-        --input_audio_filepatterns=" + AUDIO_FILEPATTERN + "\
-        --output_tfrecord_path=" + TRAIN_TFRECORD + "\
-        --num_shards=10 \
-        --alsologtostderr"
-    os.system(ddsp_prepare_tfrecord_command)
-    # Copy dataset to drive for safe-keeping.
-    if DRIVE_DIR:
-        os.system("mkdir " + drive_data_dir)
-        print('Saving to {}'.format(drive_data_dir))
-        os.system("cp " + TRAIN_TFRECORD_FILEPATTERN + " \"" + drive_data_dir + "\"/")
+# Make a new dataset.
+if not glob.glob(AUDIO_FILEPATTERN):
+    raise ValueError('No audio files found. Please use the previous cell to upload.')
+
+ddsp_prepare_tfrecord_command = "ddsp_prepare_tfrecord \
+    --input_audio_filepatterns=" + AUDIO_FILEPATTERN + "\
+    --output_tfrecord_path=" + TRAIN_TFRECORD + "\
+    --num_shards=10 \
+    --alsologtostderr"
+os.system(ddsp_prepare_tfrecord_command)
+
+# Copy dataset to drive for safe-keeping.
+# if DRIVE_DIR:
+#     os.system("mkdir " + drive_data_dir)
+#     print('Saving to {}'.format(drive_data_dir))
+#     os.system("cp " + TRAIN_TFRECORD_FILEPATTERN + " \"" + drive_data_dir + "\"/")
 
 """
 Run the following command in terminal:
